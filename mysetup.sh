fe2c32316d4a313a2497f9cb4c5546efd01ef1ac
Lyall Hamilton <lyall.hamilton@gmail.com>
2018-04-05 14:04:06 +1200

    x1 additions

--- mysetup.sh
+++ mysetup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-sudo apt-get install vim curl screen htop
+sudo apt-get install \
+	vim \
+	curl \
+	screen \
+	htop \
+	pavucontrol \
+	ranger \
+
 
 ln -sb ~/dotfiles/.screenrc ~
 ln -sb ~/dotfiles/.bash_profile ~
 ln -sb ~/dotfiles/.bashrc_custom ~
-ln -sb ~/dotfiles/.vimrc ~
+#ln -sb ~/dotfiles/.vimrc ~
 ln -sb ~/dotfiles/.gitconfig ~
 ln -sb ~/dotfiles/.gitignore_global ~
 ln -sb ~/dotfiles/.i3 ~
@@ -13,3 +20,8 @@ ln -sb ~/dotfiles/.Xmodmap ~
 
 # Load bashrc custom after local bashrc
 echo "source ~/dotfiles/.bashrc_custom" >> .bashrc
+
+# Install node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install npm@latest -g
