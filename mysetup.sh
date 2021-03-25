8818deb7bdebbcac66ef924382eb0e580cb2fbf0
Lyall Hamilton <lyall.hamilton@gmail.com>
2021-03-25 13:24:55 +1300

    Vscode settings

--- mysetup.sh
+++ mysetup.sh
@@ -3,10 +3,12 @@
 sudo apt-get install \
 	vim \
 	curl \
+	make \
 	screen \
 	htop \
 	pavucontrol \
 	ranger \
+	xbacklight \
 
 
 ln -sb ~/dotfiles/.screenrc ~
@@ -19,9 +21,13 @@ ln -sb ~/dotfiles/.i3 ~
 ln -sb ~/dotfiles/.Xmodmap ~
 
 # Vscode config
+mkdir -p .config/Code/User
 ln -s ~/dotfiles/.config/Code/User/settings.json ~/.config/Code/User
 ln -s ~/dotfiles/.config/Code/User/keybindings.json ~/.config/Code/User
 ln -s ~/dotfiles/.config/Code/User/snippets/ ~/.config/Code/User
+ln -s ~/dotfiles/.config/Code/User/settings.json ~/'.config/Code - Insiders/User'
+ln -s ~/dotfiles/.config/Code/User/keybindings.json ~/'.config/Code - Insiders/User'
+ln -s ~/dotfiles/.config/Code/User/snippets/ ~/'.config/Code - Insiders/User'
 
 # Load bashrc custom after local bashrc
 echo "source ~/dotfiles/.bashrc_custom" >> .bashrc
