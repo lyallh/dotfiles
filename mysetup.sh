f4e0f67ebe11bf5ad5e38252e20460b9b61a8c19
Lyall Hamilton <lyall.hamilton@gmail.com>
2021-04-21 14:48:47 +1200

    hide i3bar and setup script change

--- mysetup.sh
+++ mysetup.sh
@@ -9,7 +9,27 @@ sudo apt-get install \
 	ranger \
 	xbacklight \
 
+# Install node
+curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install npm@latest -g
 
+# Vscode config
+mkdir -p ../.config/Code/User
+mkdir -p '../.config/Code - Insiders/User'
+mkdir -p ~/dotfiles/.config/Code/User/snippets
+ln -s ~/dotfiles/.config/Code/User/settings.json ~/.config/Code/User
+ln -s ~/dotfiles/.config/Code/User/keybindings.json ~/.config/Code/User
+ln -s ~/dotfiles/.config/Code/User/snippets/ ~/.config/Code/User
+ln -s ~/dotfiles/.config/Code/User/settings.json ~/'.config/Code - Insiders/User'
+ln -s ~/dotfiles/.config/Code/User/keybindings.json ~/'.config/Code - Insiders/User'
+ln -s ~/dotfiles/.config/Code/User/snippets/ ~/'.config/Code - Insiders/User'
+
+# Install Vscode
+sudo snap install --classic code
+sudo snap install --classic code-insiders
+
+# Config symlinks
 ln -sb ~/dotfiles/.screenrc ~
 ln -sb ~/dotfiles/.bash_profile ~
 ln -sb ~/dotfiles/.bashrc_custom ~
@@ -19,19 +39,6 @@ ln -sb ~/dotfiles/.gitignore_global ~
 ln -sb ~/dotfiles/.i3 ~
 ln -sb ~/dotfiles/.Xmodmap ~
 
-# Vscode config
-mkdir -p ../.config/Code/User
-mkdir -p '../.config/Code - Insiders/User'
-ln -s ~/dotfiles/.config/Code/User/settings.json ~/.config/Code/User
-ln -s ~/dotfiles/.config/Code/User/keybindings.json ~/.config/Code/User
-ln -s ~/dotfiles/.config/Code/User/snippets/ ~/.config/Code/User
-ln -s ~/dotfiles/.config/Code/User/settings.json ~/'.config/Code - Insiders/User'
-ln -s ~/dotfiles/.config/Code/User/keybindings.json ~/'.config/Code - Insiders/User'
-ln -s ~/dotfiles/.config/Code/User/snippets/ ~/'.config/Code - Insiders/User'
 # Load bashrc custom after local bashrc
 echo "source ~/dotfiles/.bashrc_custom" >> ~/.bashrc
 
-# Install node
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install npm@latest -g
